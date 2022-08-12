16f66bfc07dd21d2ddfb3e48898a4f3e5e7b2abd
Praveen Mistry <praveensuthar108@gmail.com>
2022-08-12 22:14:22 +0530

    1 to 4 leetCode problem

---
+++ Add Two Numbers/add-two-numbers.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    let list = new ListNode(0);
+    let currentNode = list;
+    let sum = 0;
+    let carry = 0;
+    while(l1!=null || l2!= null || sum > 0){
+        if(l1 != null){
+           sum+=l1.val;
+            l1 = l1.next;
+        }
+        if(l2 != null){
+           sum+=l2.val;
+           l2 = l2.next;
+        }
+        
+        carry = Math.floor(sum / 10);;
+        sum = sum%10;
+        currentNode.next = new ListNode(sum);
+        currentNode = currentNode.next;
+        
+        sum = carry;
+        carry = 0;
+
+    }
+    return list.next;
+};
